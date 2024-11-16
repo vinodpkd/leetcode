7561f9ab51aef24e9d853d2157daecc6727e5d2d
vinodpkd <vinodxx@gmail.com>
2024-11-16 10:36:54 +0530

    Create 0028.cpp

---
+++ 0028.cpp
@@ -0,0 +1,7 @@
+//28. Find the Index of the First Occurrence in a String
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
