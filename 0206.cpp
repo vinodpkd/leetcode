1ce68e45940b42bad9b87c8ba221a190f5e6ba4d
vinodpkd <vinodxx@gmail.com>
2024-11-19 06:30:20 +0530

    Update 0206.cpp

--- 0206.cpp
+++ 0206.cpp
@@ -46,3 +46,38 @@ public:
         
     }
 };
+
+//Reverse a linked list
+//https://www.interviewbit.com/blog/reverse-a-linked-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !(head->next))  return head;
+    auto res = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return res;
+        
+    }
+};
+//Iterative method
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL, *cur=head, *tmp;
+        while(cur){
+            tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+   }
