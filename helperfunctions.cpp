4030e004284c5e61197208158b631ea72a2ded8f
vinodpkd <vinodxx@gmail.com>
2024-11-19 06:16:42 +0530

    Update helperfunctions.cpp

--- helperfunctions.cpp
+++ helperfunctions.cpp
@@ -174,3 +174,26 @@ struct ListNode* createLL_from_set(std::multiset<int>& A)
 
     return one;
 }
+//Reverse a linked list
+//https://www.interviewbit.com/blog/reverse-a-linked-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !(head->next))  return head;
+    auto res = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return res;
+        
+    }
+};
