0f1ba2d021ab10423c946c88b6c2ac181be27289
vinodpkd <vinodxx@gmail.com>
2024-11-19 06:26:09 +0530

    Update helperfunctions.cpp

--- helperfunctions.cpp
+++ helperfunctions.cpp
@@ -197,3 +197,14 @@ public:
         
     }
 };
+//Iterative method
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL, *cur=head, *tmp;
+        while(cur){
+            tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+   }
