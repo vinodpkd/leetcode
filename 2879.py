af383a707265255c42ae60db20cf8251b551152d
vinodpkd <vinodxx@gmail.com>
2024-11-19 08:53:25 +0530

    Create 2879.py

---
+++ 2879.py
@@ -0,0 +1,8 @@
+#2879. Display the First Three Rows
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    #return employees.head(3)
+    #return employees.iloc[0:3] 
+    return employees.iloc[[0,1,2]] 
+    
