1048e162da2d37339dd5860bcf6e01f9200ff8d3
vinodpkd <vinodxx@gmail.com>
2024-11-20 16:07:02 +0530

    Create 2235.cpp

---
+++ 2235.cpp
@@ -0,0 +1,8 @@
+//2235. Add Two Integers
+//Given two integers num1 and num2, return the sum of the two integers.  Simplest silly question in leetcode?
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
