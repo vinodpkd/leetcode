73a5ce087f03f717e2c852243bf0c73059d18019
vinodpkd <vinodxx@gmail.com>
2024-11-20 16:08:28 +0530

    Create 2413.cpp

---
+++ 2413.cpp
@@ -0,0 +1,12 @@
+//2413. Smallest Even Multiple
+//Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n. 
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n % 2 == 0)
+            return n;
+        else
+            return 2*n;
+    }
+};
+
