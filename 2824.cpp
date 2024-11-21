520b141b97cc7b94e7548423a227b7d2a1a87134
vinodpkd <vinodxx@gmail.com>
2024-11-21 15:49:54 +0530

    Create 2824.cpp

---
+++ 2824.cpp
@@ -0,0 +1,21 @@
+//2824. Count Pairs Whose Sum is Less than Target
+/*
+Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target. 
+*/
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count{0};
+        for(int i = 0;i < nums.size();i++)
+        {
+            for(int j = i+1;j < nums.size();j++)
+            {
+                if((nums[i] +  nums[j]) < target)
+                    {//std::cout << nums[i] << ' ' << nums[j] << ' ' << i << ' ' << j << '\n';
+                    count++;}
+            }
+        }
+
+        return count;
+    }
+};
