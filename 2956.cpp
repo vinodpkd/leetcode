0acfb487814cfc5826428486b63005ebe2176626
vinodpkd <vinodxx@gmail.com>
2024-11-28 14:09:48 +0530

    Update 2956.cpp

--- 2956.cpp
+++ 2956.cpp
@@ -4,22 +4,37 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+
+        int answer1{0};
+
+        for(int i = 0;i < nums1.size();i++)
+        {
+            answer1 += sum_all_indices(nums2,nums1[i]);
+        }
+
+        int answer2{0};
         
-    }
-
-    int sum_all_indices(vector<int>& A,int el)
-    {
+        for(int i = 0;i < nums2.size();i++)
+        {
+            answer2 += sum_all_indices(nums1,nums2[i]);
+        }
         
-
-    std::vector<int>::iterator iter = A.begin();
-    int index_sum {0};
-    while ((iter = std::find_if(iter, A.end(), isZero)) != A.end())
-    {
-        // Do something with iter
-        index_sum += std::distance(A.begin(),iter);
-        iter++;
+        return {answer1,answer2};
     }
 
-    return index_sum;
-    }
+    int sum_all_indices(std::vector<int>& A,int el)
+{
+	std::vector<int>::iterator iter = A.begin();
+	auto cond = [el](int num){return num == el;};
+	int index_sum {0};
+	while ((iter = std::find_if(iter, A.end(), cond)) != A.end())
+	{
+		// Do something with iter
+		index_sum = 1;//std::distance(A.begin(),iter);
+		iter++;
+        break;
+	}
+
+	return index_sum;
+}
 };
