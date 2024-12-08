5399386f22f25376957e432e16d1130a01fb5bde
vinodpkd <vinodxx@gmail.com>
2024-12-08 21:04:42 +0530

    Create 1721.cpp

---
+++ 1721.cpp
@@ -0,0 +1,56 @@
+//1721. Swapping Nodes in a Linked List
+/*
+You are given the head of a linked list, and an integer k.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        std::vector<int> vec = ll2Vec(head);
+        std::cout << vec.size() << '\n';
+        std::swap(vec[k-1],vec[vec.size()-1-(k-1)]);
+        head = vec2ll(vec);
+        return head;
+    }
+
+    std::vector<int> ll2Vec(ListNode* l1)
+    {
+        std::vector<int> num{};
+        ListNode* t = l1;
+        while(t != nullptr)
+        {
+            num.push_back(t->val);
+            t = t->next;
+        }
+        
+        return num;
+    }
+
+    ListNode* vec2ll(std::vector<int>& num)
+    {
+        
+        ListNode* t = new ListNode(num[0]);
+		ListNode* s = new ListNode();
+		s = t;
+		int i = 1;
+        while(i < num.size())
+        {            
+			ListNode* u = new ListNode(num[i]);            
+			t->next = u;
+            t = t->next;
+			i++;
+        }
+        
+        return s;
+    }
+};
