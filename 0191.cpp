0d02f4682e5080956244eae988954d36f3533b8f
vinodpkd <vinodxx@gmail.com>
2024-12-08 07:35:26 +0530

    Create 0191.cpp

---
+++ 0191.cpp
@@ -0,0 +1,12 @@
+//191. Number of 1 Bits
+/*
+Given a positive integer n, write a function that returns the number of
+set bits
+in its binary representation (also known as the Hamming weight).
+*/
+class Solution {
+public:
+    int hammingWeight(int n) {
+        return __builtin_popcount(n);
+    }
+};
