b331ae14a0262a2dd1fdaca0fde4217021cd4739
vinodpkd <vinodxx@gmail.com>
2024-12-08 21:48:12 +0530

    Create 0025.cpp

---
+++ 0025.cpp
@@ -0,0 +1,70 @@
+//25. Reverse Nodes in k-Group
+/*
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (1 == k)
+            return head;
+
+        std::vector<int> vec = ll2Vec(head);
+
+        if(k > vec.size())
+            return head;
+
+        int size = vec.size();
+        if(size % k != 0)
+            size = size - (size % k);
+
+        for(int i = 0;i < size;i = i + k)
+        {
+            std::reverse(vec.begin()+i,vec.begin()+i+k);
+        }
+
+        return vec2ll(vec);
+    }
+
+     std::vector<int> ll2Vec(ListNode* l1)
+    {
+        std::vector<int> num{};
+        ListNode* t = l1;
+        while(t != nullptr)
+        {
+            num.push_back(t->val);
+            t = t->next;
+        }
+        
+        return num;
+    }
+
+    ListNode* vec2ll(std::vector<int>& num)
+    {
+        
+        ListNode* t = new ListNode(num[0]);
+		ListNode* s = new ListNode();
+		s = t;
+		int i = 1;
+        while(i < num.size())
+        {            
+			ListNode* u = new ListNode(num[i]);            
+			t->next = u;
+            t = t->next;
+			i++;
+        }
+        
+        return s;
+    }
+};
