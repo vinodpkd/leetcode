e61c890d2ade25cbcc2d22189314d6a799e6feb7
vinodpkd <vinodxx@gmail.com>
2024-12-09 10:43:31 +0530

    Create 0083.cpp

---
+++ 0083.cpp
@@ -0,0 +1,58 @@
+//83. Remove Duplicates from Sorted List
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+*/
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        std::vector<int> vec = ll2Vec(head);
+        auto last = std::unique(vec.begin(),vec.end());
+        vec.erase(last,vec.end());
+        
+        head = vec2ll(vec);
+        return head;
+    }
+
+     std::vector<int> ll2Vec(ListNode* l1)
+    {
+        std::vector<int> num{};
+        ListNode* t = l1;
+        while(t != nullptr)
+        {
+            num.push_back(t->val);
+            t = t->next;
+        }
+        
+        return num;
+    }
+
+    ListNode* vec2ll(std::vector<int>& num)
+    {
+	    if(num.empty())
+            	return nullptr;
+        
+        ListNode* t = new ListNode(num[0]);
+		ListNode* s = new ListNode();
+		s = t;
+		int i = 1;
+        while(i < num.size())
+        {            
+			ListNode* u = new ListNode(num[i]);            
+			t->next = u;
+            t = t->next;
+			i++;
+        }
+        
+        return s;
+    }
+};
