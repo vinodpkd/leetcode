76b86d7ce375ae68e150d5bd3d405196b3103ec0
vinodpkd <vinodxx@gmail.com>
2024-12-10 09:40:39 +0530

    Create 0092.cpp

---
+++ 0092.cpp
@@ -0,0 +1,57 @@
+//92. Reverse Linked List II
+/*
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head->next == nullptr)
+            return head;
+        std::vector<int> vec = ll2vec(head);
+        std::reverse(vec.begin()+(left-1),vec.begin()+right);
+        return vec2ll(vec);
+    }
+
+     std::vector<int> ll2vec(ListNode* l1)
+    {
+        std::vector<int> num{};
+        ListNode* t = l1;
+        while(t != nullptr)
+        {
+            num.push_back(t->val);
+            t = t->next;
+        }
+        
+        return num;
+    }
+
+    ListNode* vec2ll(std::vector<int>& num)
+    {
+	    if(num.empty())
+            	return nullptr;
+        
+        ListNode* t = new ListNode(num[0]);
+		ListNode* s = new ListNode();
+		s = t;
+		int i = 1;
+        while(i < num.size())
+        {            
+			ListNode* u = new ListNode(num[i]);            
+			t->next = u;
+            t = t->next;
+			i++;
+        }
+        
+        return s;
+    }
+};
