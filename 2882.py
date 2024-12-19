65237f82435817b73c5b0d9d24cfba1a16aa0ff1
vinodpkd <vinodxx@gmail.com>
2024-12-19 11:47:06 +0530

    Create 2882.py

---
+++ 2882.py
@@ -0,0 +1,10 @@
+//2882. Drop Duplicate Rows
+/*
+There are some duplicate rows in the DataFrame based on the email column.
+Write a solution to remove these duplicate rows and keep only the first occurrence.
+*/
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset=['email'])
+    
