c1e37acf88ad9aa4aeb3088f1b6637903d43377b
vinodpkd <vinodxx@gmail.com>
2024-12-21 19:50:08 +0530

    Create 0242.cpp

---
+++ 0242.cpp
@@ -0,0 +1,22 @@
+//242. Valid Anagram
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise. 
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //std::multiset<char> u(s.begin(),s.end());
+        //std::multiset<char> v(t.begin(),t.end());
+        
+        //if(u == v)
+        //    return true;
+
+        std::sort(s.begin(),s.end());
+        std::sort(t.begin(),t.end());
+
+        if(s == t)
+            return true;
+
+        return false;
+    }
+};
