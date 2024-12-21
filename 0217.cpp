ba0e98bacaf56ac9192a47880103971adeab9e0a
vinodpkd <vinodxx@gmail.com>
2024-12-21 19:26:12 +0530

    Update 0217.cpp

--- 0217.cpp
+++ 0217.cpp
@@ -1,7 +1,24 @@
 //217 Contains duplicate
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+        int size = nums.size();
+        std::sort(nums.begin(),nums.end());
+        auto it = std::unique(nums.begin(),nums.end());
+        nums.erase(it,nums.end());
+        //for(int& x : nums)
+        //    std::cout << x << ' ';
+        //std::cout << '\n';
+        if(size == nums.size())
+            return false;
+        
+        return true;
+       /*
+            
         std::map<int,int> dict {};
         for(int i = 0;i < nums.size();i++)
         {
@@ -11,6 +28,7 @@ public:
         }
 
         return false;
+        */
         
     }
 };
