891474d50cb28e178b294a4f8cbe3bd6c583a92e
vinodpkd <vinodxx@gmail.com>
2024-12-23 19:09:07 +0530

    Update 0633.cpp

--- 0633.cpp
+++ 0633.cpp
@@ -68,3 +68,19 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long left = 0;
+        long long right = sqrt(c);
+        
+        while (left <= right) {
+            long long sum = left * left + right * right;
+            if (sum == c) return true;
+            if (sum < c) left++;
+            else right--;
+        }
+        return false;
+    }
+};
+
