142651149074c4514a56c1d7b77ef2c317b63cc8
vinodpkd <vinodxx@gmail.com>
2024-12-25 17:10:04 +0530

    Update 0560.cpp

--- 0560.cpp
+++ 0560.cpp
@@ -1,4 +1,4 @@
-//560. Subarray Sum Equals K , Brute force, timelimit exceeded
+//560. Subarray Sum Equals K , Brute force
 /*
 Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 A subarray is a contiguous non-empty sequence of elements within an array.
@@ -10,13 +10,17 @@ public:
 	
 	for(int i = 0;i < arr.size();i++)
 	{
+        int sum = arr[i];
 		for(int j = i;j < arr.size();j++)
 		{
-			int sum = std::accumulate(arr.begin()+i,arr.begin()+j+1,0);
+			//int sum = std::accumulate(arr.begin()+i,arr.begin()+j+1,0);
 			if(sum == k)
 			{
 				count++;
 			}
+            if(j+1 == arr.size())
+                break;
+            sum += arr[j+1];
 		}
 	}
     return count;
