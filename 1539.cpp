2928522a467b51c3d24a13755d4bcd1f2aef6a80
vinodpkd <vinodxx@gmail.com>
2025-01-09 15:04:50 +0530

    Create 1539.cpp

---
+++ 1539.cpp
@@ -0,0 +1,29 @@
+//1539. Kth Missing Positive Number
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+*/
+class Solution {
+public:
+    int findKthPositive(vector<int>& nums, int k) {
+        int i = 1;
+        std::vector<int> m{};
+        for(int j = 0;j < nums.size();j++)
+        {
+            for(int p = i;p < nums[j];p++)
+            {
+                m.push_back(p);
+                if(m.size() == k)
+                    return p;
+            }
+            i = nums[j]+1;
+        }
+        for(int x : m)
+            std::cout << x << ' ';
+        std::cout << '\n';
+        if(m.size() < k)
+            return nums[nums.size()-1] + (k - m.size());
+        
+        return 1;
+    }
+};
