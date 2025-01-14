bb8e4782628d0453c3c6ef7600ede42050582e5e
vinodpkd <vinodxx@gmail.com>
2025-01-14 19:32:00 +0530

    Update 0234.cpp

--- 0234.cpp
+++ 0234.cpp
@@ -57,3 +57,42 @@ public:
         return s;
     }
 };
+
+----------------------
+	/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        std::stack<int> s{};
+        ListNode* temp = head;
+        while(temp)
+        {
+            s.push(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp)
+        {
+            if(temp->val == s.top())
+            {
+                temp = temp->next;
+                s.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
