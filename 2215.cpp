febdd1da5acf94725102d02133fd99dbc05fef51
vinodpkd <vinodxx@gmail.com>
2025-01-15 16:32:44 +0530

    Update 2215.cpp

--- 2215.cpp
+++ 2215.cpp
@@ -21,3 +21,56 @@ public:
         return dist;
     }
 };
+
+-----------------
+//Two pointers
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        // Sort both arrays first for two pointer approach
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        // Remove duplicates from both arrays
+        nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+        
+        vector<int> diff1;  // Elements unique to nums1
+        vector<int> diff2;  // Elements unique to nums2
+        
+        // Two pointers
+        int i = 0, j = 0;
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                // Current element in nums1 is smaller, so it's unique to nums1
+                diff1.push_back(nums1[i]);
+                i++;
+            }
+            else if (nums1[i] > nums2[j]) {
+                // Current element in nums2 is smaller, so it's unique to nums2
+                diff2.push_back(nums2[j]);
+                j++;
+            }
+            else {
+                // Elements are equal, skip both
+                i++;
+                j++;
+            }
+        }
+        
+        // Add remaining elements from nums1
+        while (i < nums1.size()) {
+            diff1.push_back(nums1[i]);
+            i++;
+        }
+        
+        // Add remaining elements from nums2
+        while (j < nums2.size()) {
+            diff2.push_back(nums2[j]);
+            j++;
+        }
+        
+        return {diff1, diff2};
+    }
+};
