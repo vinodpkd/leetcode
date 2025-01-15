34df35685b00b9262913409dcfe44692d45c2f1d
vinodpkd <vinodxx@gmail.com>
2025-01-15 15:41:32 +0530

    Update 0350.cpp

--- 0350.cpp
+++ 0350.cpp
@@ -9,3 +9,44 @@ public:
         return inter;
     }
 };
+
+--------
+//Two pointers approach
+class Solution {
+public:
+    vector<int> intersect(vector<int>& a, vector<int>& b) {
+        std::ranges::sort(a);
+        std::ranges::sort(b);
+        std::vector<int> res{};
+	
+	int i = 0;
+	int j = 0;
+	
+	while(i < a.size() && j < b.size())
+	{
+		if(a[i] > b[j])
+		{
+			j++;
+			continue;
+		}
+		
+		if(a[i] == b[j])
+		{
+			
+					res.push_back(a[i]);
+			
+			
+			i++;
+			j++;
+			continue;			
+		}
+		
+		if(a[i] < b[j])
+		{
+			i++;			
+		}
+	}
+
+    return res;
+    }
+};
