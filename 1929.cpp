544a95ef68fcf4a3d6dda6e2737fcf2e03cc35f2
vinodpkd <vinodxx@gmail.com>
2025-01-23 10:00:06 +0530

    Update 1929.cpp

--- 1929.cpp
+++ 1929.cpp
@@ -13,3 +13,11 @@ public:
         //return nums;
     }
 };
+-------------
+ class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        return nums;
+    }
+};
