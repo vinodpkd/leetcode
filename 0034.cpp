1fa46f9363791cf06e2cea7979f8f145d78de46f
vinodpkd <vinodxx@gmail.com>
2025-01-25 10:01:38 +0530

    Update 0034.cpp

--- 0034.cpp
+++ 0034.cpp
@@ -24,3 +24,49 @@ public:
         }
     }
 };
+
+-----------------
+100% beats in time
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size(); //size of the array
+        if(0 == n)
+            return {-1,-1};
+    int low = 0, high = n - 1;
+	int mid;
+    // Perform the steps:
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (nums[mid] == target) 
+		{
+			int start = mid;
+			
+			if(start-1 >= 0 && nums[start-1] == target)
+			{
+				start--;
+				
+				while(start-1 >= 0 && nums[start-1] == target)
+				{
+					start--;
+				}
+			}
+			
+			if(mid+1 < nums.size() && nums[mid+1] == target)
+			{
+				mid++;
+				
+				while(mid+1 < nums.size() && nums[mid+1] == target)
+				{
+					mid++;
+				}
+			}
+			return {start,mid};
+		}
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    //
+	return {-1,-1};
+    }
+};
