601180d626b71c4c8a0b740a3186ddb96d280261
vinodpkd <vinodxx@gmail.com>
2025-01-28 10:50:04 +0530

    Update 0704.cpp

--- 0704.cpp
+++ 0704.cpp
@@ -35,3 +35,15 @@ public:
 	return -1;
     }
 };
+-----------------
+Solution with STL
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       auto it = std::lower_bound(nums.begin(),nums.end(),target); 
+       if(it != nums.end() && *it == target)
+            return std::distance(nums.begin(),it);
+        return -1;
+    }
+};
+	
