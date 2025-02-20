d6889d49c22ab767b27b3fdbe666226bce759c16
vinodpkd <vinodxx@gmail.com>
2025-02-20 17:18:03 +0530

    Update 0710.cpp
    a

--- 0710.cpp
+++ 0710.cpp
@@ -55,3 +55,41 @@ public:
  * Solution* obj = new Solution(n, blacklist);
  * int param_1 = obj->pick();
  */
+-----------------
+    class Solution {
+private:
+    int n;
+    unordered_map<int, int> mapping;
+    int validRange;
+    
+public:
+    Solution(int n, vector<int>& blacklist) {
+        this->n = n;
+        this->validRange = n - blacklist.size();
+        
+        // Create set of blacklisted numbers
+        unordered_set<int> blackSet(blacklist.begin(), blacklist.end());
+        
+        // Find valid numbers after validRange
+        int validNum = validRange;
+        for (int blackNum : blacklist) {
+            // If blacklisted number is in our valid range
+            if (blackNum < validRange) {
+                // Find next valid number to map to
+                while (blackSet.count(validNum)) {
+                    validNum++;
+                }
+                mapping[blackNum] = validNum++;
+            }
+        }
+    }
+    
+    int pick() {
+        // Generate random number in valid range
+        int randNum = rand() % validRange;
+        
+        // If this number was blacklisted, return its mapping
+        // Otherwise return the number itself
+        return mapping.count(randNum) ? mapping[randNum] : randNum;
+    }
+};
